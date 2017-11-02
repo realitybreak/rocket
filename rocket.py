9c121092b980b13d78fa9fcdf830dd8b04bad608
Mario Garcia <mario@reality-break.com>
2017-11-02 12:51:13 -0700

    fixed set_mode for display

--- rocket.py
+++ rocket.py
@@ -5,7 +5,7 @@ import pygame
 def run_game():
     # Initialize game and create a screen object.
     pygame.init()
-    screen = pygame.display.set mode ((1200, 800))
+    screen = pygame.display.set_mode ((1200, 800))
     pygame.display.set_caption("Rocket")
 
     # Start the main loop for the game.
