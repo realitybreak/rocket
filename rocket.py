9efcc50c406361092b70b584ec26fa093aa324e5
Mario Garcia <mario@reality-break.com>
2017-11-02 15:30:36 -0700

    updates work for work

--- rocket.py
+++ rocket.py
@@ -2,21 +2,25 @@ import sys
 
 import pygame
 
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
 def run_game():
-    # Initialize game and create a screen object.
+    # Initialize pygame, settings, and screen object.
     pygame.init()
-    screen = pygame.display.set_mode ((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Rocket")
+
+    # Make a ship.
+    ship = Ship(ai_settings, screen)
 
     # Start the main loop for the game.
     while True:
-
-        #Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()
---
+++ settings.py
@@ -0,0 +1,12 @@
+class Settings():
+    """A Class to store all settings for Rocket."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (0, 0, 0)
+
+        # Ship settings
+        self.ship_speed_factor = 1.5
